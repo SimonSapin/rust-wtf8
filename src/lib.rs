1f782eb460d979e97ec6057131599c3f09bedcda
Simon Sapin <simon.sapin@exyr.org>
2014-10-03 09:09:32 +0100

    In the beginning was an empty library.

---
+++ src/lib.rs
@@ -0,0 +1,9 @@
+/*!
+
+**rust-wtf8** is an implementation of [the WTF-8 encoding](http://simonsapin.github.io/wtf-8/).
+
+It uses Rust’s type system to maintain
+[well-formedness](http://simonsapin.github.io/wtf-8/#well-formed),
+like the `String` and `&str` types do for UTF-8.
+
+*/
